3b837e5039f2bc249d2ac8372d77ba357c3a26cf
FaizanMuhammadSE <jutt41697@gmail.com>
2024-10-11 11:26:41 +0500

    module-federation added

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,24 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-
+import federation from '@originjs/vite-plugin-federation';
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    federation({
+      name: 'sharedComponents',
+      filename: 'remoteEntry.js',
+      exposes: {
+        './Card': './src/components/Card/index.tsx',
+      },
+      shared: ['react', 'react-dom'],
+    }),
+  ],
   build: {
+    modulePreload: false,
+    target: 'esnext',
     manifest: true,
+    minify: false,
+    cssCodeSplit: false,
   },
 });
